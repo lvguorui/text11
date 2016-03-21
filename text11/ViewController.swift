1a316c5aa090ed23381b6460d14c59bd085499c3
lvguorui <lvguorui@admindeiMac-33.local>
2016-03-21 21:15:04 +0800

    jisuanqi
    jisuanq

---
+++ text11/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  text11
+//
+//  Created by lvguorui on 16/3/21.
+//  Copyright © 2016年 lvguorui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
